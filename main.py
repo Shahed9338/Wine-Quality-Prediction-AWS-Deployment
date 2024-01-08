738dcc7638639fe53fba3cbfec6bd14bc09ed8a9
Shahed9338 <shahediqbal93@gmail.com>
2024-01-08 19:20:15 +0100

    Data Ingestion Done

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from wineQuality.logging import logger
+from wineQuality.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Logging initialized successfully")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
