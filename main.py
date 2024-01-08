b510bae6c9de848bdcb0dacada018f5baaf209b7
Shahed9338 <shahediqbal93@gmail.com>
2024-01-08 23:15:11 +0100

    Model Training Done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from wineQuality.logging import logger
 from wineQuality.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from wineQuality.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from wineQuality.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
-
+from wineQuality.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -30,6 +30,18 @@ STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
